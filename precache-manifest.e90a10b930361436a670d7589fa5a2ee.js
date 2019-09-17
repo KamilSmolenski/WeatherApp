3958fcda89615889cbf98c61a4d69635d2f54882
kamilsmolen.ski <smolesnkikamil@gmail.com>
2019-09-17 22:23:54 +0200

    Updates

---
+++ precache-manifest.e90a10b930361436a670d7589fa5a2ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a6a07bfaa06719deae99b92f3f70ae",
+    "url": "/WeatherApp/./index.html"
+  },
+  {
+    "revision": "8a2a35220f60fd822142",
+    "url": "/WeatherApp/./static/css/main.68a61ccb.chunk.css"
+  },
+  {
+    "revision": "a3846173083bcacb5b91",
+    "url": "/WeatherApp/./static/js/2.831c99be.chunk.js"
+  },
+  {
+    "revision": "8a2a35220f60fd822142",
+    "url": "/WeatherApp/./static/js/main.cdea6b76.chunk.js"
+  },
+  {
+    "revision": "aa3195f3f5e049c286d2",
+    "url": "/WeatherApp/./static/js/runtime~main.658f919c.js"
+  }
+]);
