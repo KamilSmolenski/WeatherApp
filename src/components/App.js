6e9f89fc31e5d55a5ae330e2e8dd66a8f526e323
kamilsmolen.ski <smolesnkikamil@gmail.com>
2019-09-19 14:01:52 +0200

    add weather icons and description, correct sunrise and sunset time format, round wind number to integer

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,51 @@
-import React, { Component } from 'react'
-import './App.css'
-import Results from './Results'
-import Search from './Search'
+import React, { Component } from "react";
+import "./App.css";
+import Results from "./Results";
+import Search from "./Search";
 
 class App extends Component {
   state = {
-    value: '',
-    time: '',
-    weekDay: '',
-    date: '',
-    city: '',
-    sunrise: '',
-    sunset: '',
-    wind: '',
-    pressure: '',
-    temp: '',
-    error: 'false'
-  }
+    value: "",
+    time: "",
+    weekDay: "",
+    date: "",
+    city: "",
+    sunrise: "",
+    sunset: "",
+    wind: "",
+    pressure: "",
+    temp: "",
+    description: "",
+    iconId: "",
+    iconUrl: "",
+    error: "false",
+  };
 
   handleSearchChange = e => {
     this.setState({
-      value: e.target.value
-    })
-  }
-  componentDidMount () {
-    this.setState({ value: 'Warsaw' })
-  }
-  componentDidUpdate (prevProps, prevState) {
-    const WeekDays = ['Sun.', 'Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fri.', 'Sat.']
-    const WeekDaysIndex = new Date().getDay()
-    if (this.state.value.length === 0) return
+      value: e.target.value,
+    });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const WeekDays = ["Sun.", "Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat."];
+    const WeekDaysIndex = new Date().getDay();
+    if (this.state.value.length === 0) return;
     if (prevState.value !== this.state.value) {
-      const API = `http://api.openweathermap.org/data/2.5/weather?q=${
-        this.state.value
-      }&APPID=cd2719d19d08b2909c6be691a47dad9c&units=metric`
+      const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=cd2719d19d08b2909c6be691a47dad9c&units=metric`;
       fetch(API)
         .then(response => {
           if (response.ok) {
-            return response
+            return response;
           }
-          throw Error(`number ${response.status}`)
+          throw Error(`number ${response.status}`);
         })
         .then(response => response.json())
         .then(data => {
-          const currentTime = `${new Date().getHours()}:${new Date().getMinutes()} `
-          const currentDate = new Date().toLocaleDateString()
+          const currentTime = new Date(data.dt * 1000).toLocaleTimeString();
+          const currentDate = new Date().toLocaleDateString();
+          const windSpeed = data.wind.speed * 3.6;
+          const windSpeedInteger = Math.round(windSpeed);
           this.setState({
             error: false,
             weekDay: WeekDays[WeekDaysIndex],
@@ -53,28 +54,33 @@ class App extends Component {
             city: this.state.value,
             sunrise: data.sys.sunrise,
             sunset: data.sys.sunset,
-            wind: data.wind.speed * 3.6 + ' km/h',
+            wind: windSpeedInteger + " km/h",
             pressure: data.main.pressure,
-            temp: data.main.temp
-          })
+            temp: data.main.temp,
+            description: ` ${data.weather[0].description}`,
+            iconId: data.weather[0].icon,
+          });
+          this.setState({
+            iconUrl: ` http://openweathermap.org/img/wn/${this.state.iconId}@2x.png`,
+          });
         })
         .catch(err => {
           this.setState({
             error: true,
-            city: this.state.value
-          })
-        })
+            city: this.state.value,
+          });
+        });
     }
   }
 
-  render () {
+  render() {
     return (
-      <div className='App'>
+      <div className="App">
         <Search value={this.state.value} change={this.handleSearchChange} />
         <Results currentWeather={this.state} />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import './Results.css'
+import React from "react";
+import "./Results.css";
 
 const Results = props => {
   const {
@@ -12,31 +12,43 @@ const Results = props => {
     wind,
     pressure,
     temp,
-    error
-  } = props.currentWeather
+    description,
+    error,
+    iconUrl,
+  } = props.currentWeather;
 
-  let content = null
+  let content = null;
   if (!error && city) {
-    const sunsetTime = new Date(sunset * 1000).toLocaleTimeString()
-    const sunriseTime = new Date(sunrise * 1000).toLocaleTimeString()
+    const sunsetTime = `${new Date(sunset * 1000).getHours()}:${
+      new Date(sunset * 1000).getMinutes() < 10
+        ? "0" + new Date(sunset * 1000).getMinutes()
+        : new Date(sunset * 1000).getMinutes()
+    }`;
+    const sunriseTime = `${new Date(sunrise * 1000).getHours()}:${
+      new Date(sunrise * 1000).getMinutes() < 10
+        ? "0" + new Date(sunrise * 1000).getMinutes()
+        : new Date(sunrise * 1000).getMinutes()
+    }`;
 
     content = (
-      <div className='Results'>
+      <div className="Results">
         <h3>
           City: <strong>{city}</strong>
         </h3>
+        <img src={iconUrl} alt="Current weather icon" />
         <h4>{time}</h4>
         <h4>{weekDay}</h4>
         <h4>{date}</h4>
+        <h4>Desc:{description}</h4>
         <h4>Temp:{temp} &#176;C</h4>
         <h4>Sunrise: {sunriseTime}</h4>
         <h4>Sunset: {sunsetTime}</h4>
         <h4>Wind:{wind}</h4>
-        <h4>Pressure:{pressure} hPa</h4>
+        <h4>Pressure:{Math.round(pressure)} hPa</h4>
       </div>
-    )
+    );
   }
-  return <div className='results'>{error ? `Not found ${city}` : content}</div>
-}
+  return <div className="results">{error ? `Not found ${city}` : content}</div>;
+};
 
-export default Results
+export default Results;
