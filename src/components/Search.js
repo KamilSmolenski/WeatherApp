222b944d09ad2daa07c10bfdd3e0bb8528c33080
kamilsmolen.ski <smolenskikamil@gmail.com>
2019-11-05 13:28:31 +0100

    fix search bug

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,26 +1,29 @@
-import React from 'react'
-import './Search.scss'
-import ReactTypingEffect from 'react-typing-effect'
+import React from "react";
+import "./Search.scss";
+import ReactTypingEffect from "react-typing-effect";
+function handleSubmit(e) {
+  e.preventDefault();
+}
 const Search = props => {
   return (
-    <div className='SearchPanel'>
+    <div className="SearchPanel">
       <ReactTypingEffect
         eraseDelay={3000}
         speed={200}
-        text={['for today.', 'forecast for tomorrow.', 'for next 5 days.']}
-        staticText='Get weather'
-        className='Typing'
+        text={["for today.", "forecast for tomorrow.", "for next 5 days."]}
+        staticText="Get weather"
+        className="Typing"
       />
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
-          type='text'
+          type="text"
           value={props.value}
-          placeholder='Find a city'
+          placeholder="Find a city"
           onChange={props.change}
         />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
